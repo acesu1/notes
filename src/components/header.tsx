8ad0325fbda9f4786656533fb941110741aa08b9
Douglas Souza <devdouglassouza2002@hotmail.com>
2024-11-17 12:22:38 -0300

    feat: add authentication with clerk and using convex as db

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,23 @@
 import { NotebookPen } from 'lucide-react'
 import { ThemeToggle } from './theme-toggle'
+import { SignInButton, UserButton } from '@clerk/clerk-react'
+import { Unauthenticated, Authenticated } from 'convex/react'
 
 export function Header() {
   return (
     <div className="border-b">
       <div className="flex h-16 items-center justify-between px-8">
         <NotebookPen />
-        <ThemeToggle />
+
+        <div className="flex items-center gap-4">
+          <Unauthenticated>
+            <SignInButton />
+          </Unauthenticated>
+          <Authenticated>
+            <UserButton />
+          </Authenticated>
+          <ThemeToggle />
+        </div>
       </div>
     </div>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,26 @@ import './index.css'
 import { App } from './App.tsx'
 
 import { ThemeProvider } from '@/components/theme-provider'
+import { ConvexReactClient } from 'convex/react'
+import { ClerkProvider, useAuth } from '@clerk/clerk-react'
+import { ConvexProviderWithClerk } from 'convex/react-clerk'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
+
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+        <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+          <App />
+        </ConvexProviderWithClerk>
+      </ClerkProvider>
     </ThemeProvider>
   </StrictMode>
 )
